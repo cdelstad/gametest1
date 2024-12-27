c6ea6e10d56958508c8ba6a4ef45f43cd67d34b6
Cjad Elstad <cde008@gmail.com>
2024-12-26 23:51:46 -0600

    progress on ui manager class

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,65 @@
 import * as ex from 'excalibur';
 // import { loader } from './InitddialResources';
 import DemoScene from '../src/scenes/DemoScene';
+import { uiManager } from './utils/UIManager';
+
+const uiMgr = uiManager.initRegistry({baseZIndex: 1000});
+
+uiMgr.create({   
+    id: 'test1',
+    worldPos: {x:1,y:1},
+    screenPos: {x:10,y:10},
+    isComposite: false,
+    // children?: Element[],
+    isVisible: true, 
+    isReady: true, 
+    zIndex: 10, 
+    pointerEvent: 'all' 
+});
+
+uiMgr.create({   
+    id: 'test2',
+    worldPos: {x:1,y:1},
+    screenPos: {x:40,y:10},
+    isComposite: false,
+    // children?: Element[],
+    isVisible: true, 
+    isReady: true, 
+    zIndex: 10, 
+    pointerEvent: 'all' 
+});
+    
+console.log('getElement',uiMgr.getElement('test1'));
+
+// uiMgr.remove('test2');
+
+//uiMgr.clearRegistry();
+
+const button1 = uiMgr.getElement('test1');
+// We want to use the UI manager to create mechanism for devs to modify, perhaps signals, though since it is HTML, a developer can modify it as they want.
+setTimeout(() => {
+    button1!.innerText = "Don't click me!";
+    button1!.ariaLabel = "Don't click me, ARIA!";
+}, 2000);
+// button1!.style.left = "100px";
+
+
+const button2 = document.createElement("button");
+button2.innerText = 'Click me2!';
+button2.id = 'test3';
+button2.style.position = "absolute";
+button2.style.top = "100px";
+button2.style.left = "100px";
+// button2.style.display = "none";
+// button2.style.visibility = "hidden";
+document.body.appendChild(button2);
+
+uiMgr.register(button2!);
+
+console.log(uiMgr.getRegistry());
+
+// console.log("isVisible",uiMgr.calcVisibility(button2));
+
 
 const game = new ex.Engine({
     width: 800,
--- src/utils/UIManager.ts
+++ src/utils/UIManager.ts
@@ -1,59 +1,63 @@
-// import * as ex from 'excalibur'
-
 interface Position {
-    x: Number,
-    y: Number,
-    zIndex?: Number // Should allow a user to set/update a z-index? Or do we allow this to be set separately like setZIndex()?
+    x: number,
+    y: number,
+    zIndex?: number // Should allow a user to set/update a z-index? Or do we allow this to be set separately like setZIndex()?
 };
 
-// TODO Confirm this is the correct way to enforce the callback function provided takes an x and y param. How to enforce it returns an x/y?
-type PosCallback = (x: Number, y: Number) => any;
+// TODO Confirm this is the correct way to enforce the callback function provided takes an x and y param. How to enforce it returns an x/y? is a vector
+type PosCallback = (x: number, y: number) => any;
 
 interface Element {
-    id: String, // Element ID
-    ref: HTMLElement, // reference to the element in the DOM
+    id: string, // Element ID
+    ref?: HTMLElement, // reference to the element in the DOM
     worldPos: Position, // position on the canvas/world space
     screenPos: Position, // position on the screen
-    isComposite: Boolean, // Is this element a composite
-    children?: Element[], // Can I do this? this will be used to organize children in composite elements.
-    isVisible: Boolean, // Should this be visible on the screen?
-    isReady: Boolean, // Is the element ready to be used? Set default in create/register functions.
-    zIndex: Number, // To allow for control over layering, especially on composite elements
-    pointerEvent: String // This sets behavior on whether pointer events should passthrough to the canvas?
+    isComposite: boolean, // Is this element a composite
+    // children?: Element[], // Can I do this? this will be used to organize children in composite elements.
+    isVisible: boolean, // Should this be visible on the screen?
+    isReady: boolean, // Is the element ready to be used? Set default in create/register functions.
+    zIndex: number, // To allow for control over layering, especially on composite elements
+    pointerEvent: string // This sets behavior on whether pointer events should passthrough to the canvas?
 };
 
 interface Config {
     // TODO should these callbacks accept a return value of a vector or object instead of idividual values? {x,y}?
-    baseZIndex: Number,
-    worldToScreenCallback: PosCallback, // This is the function used to translate from world to screen coordinates
-    screenToWorldCallback: PosCallback, // This is the function used to translate from screen to world coordinates
+    baseZIndex: number,
+    // TODO make these required once I get most of this class completed.
+    worldToScreenCallback?: PosCallback, // This is the function used to translate from world to screen coordinates
+    screenToWorldCallback?: PosCallback, // This is the function used to translate from screen to world coordinates
     
 }
 
 class UIManager {
-    private registry: Element[] | null = null
+    // TODO is this the right way to initialize this?
+    registry: Element[] = [];
+    instance!: UIManager;
 
     initRegistry(config: Config) {
         // TODO Check for aria-live-root and if not found, add it and setup this region
-        // add logic to make this a singleton
-        // if(!this.registry) { this.registry = Element[] }
-        // this.engine = engine;
-
+        // The UI Manager needs to be a singleton.
+        // TODO should I be checking that instance is a typeof UIManager? Need to confirm that this will always be an object and only return from the if statement. 
+        // It seems to be working when I try to create another UIManager.
+        if (typeof this.instance === 'object'){ return this.instance;}
+        this.instance = this;
+        return this.instance;
     }
 
     // Return current UI registry
     getRegistry() {
-
+        return this.registry;
     };
 
     // reset the registry to initial empty state
     clearRegistry() {
-
+        // TODO not sure this is the right way to do this with TypeScript.
+        this.registry = [];
     }
 
-    // Return DOM element ref from registry by string?
+    // Return DOM element ref from registry by id
     getElement(id: String) {
-
+        return this.registry.find(x => x.id === id)?.ref;
     };
     
     // Hide all UI elements in registry
@@ -69,22 +73,56 @@ class UIManager {
     }
 
     // Creates a UI element using internal components, registers it, and adds it to the DOM
-    create() {
+    create( elem: Element) {
+
+        const button = document.createElement("button");
+        button.innerText = 'Click me!';
+        button.id = elem.id;
+        button.style.position = "absolute";
+        button.style.top = elem.screenPos.x+'px';
+        button.style.left = elem.screenPos.y+'px';
+        document.body.appendChild(button);
+
+        elem.ref = button;
+        this.registry?.push(elem);
+
     }
 
     // Remove an element by ID
-    remove() {
+    remove(id: string) {
 
+        // Remove element from DOM
+        // document.getElementById(id)?.remove();
+        this.getElement(id)?.remove();
+        // Remove element from registry array
+        const indexToRemove = this.registry.findIndex(item => item.id === id);
+        if (indexToRemove > -1) {
+            this.registry.splice(indexToRemove, 1);
+        }
     }
 
     // Registers an already created element in the DOM
-    register() {
-
+    register(elem: HTMLElement) {
+        this.registry?.push(
+            {
+                id: elem.id,
+                ref: elem,
+                worldPos: {x:1,y:1}, // TODO add calculation here with screenToWorldCallback?
+                screenPos: {x:parseInt(elem.style.top,10),y:parseInt(elem.style.left,10)},
+                isComposite: false, // need to make dynamic - do we look to see if more element children or require user to pass in this flag??
+                // children?: Element[],
+                isVisible: this._calcVisibility(elem),
+                isReady: true,  // need to make dynamic
+                zIndex: 10, // need to make dynamic
+                pointerEvent: 'all' // need to make dynamic
+            }
+        );
+        
     }
 
     // Allows you to compose your own HTML, create composite components, and then register them
     compose() {
-
+        // TODO may need to start by users creating each HTMLElement individually, but need to look at how to generate html - maybe a Lit component that uses lit HTML ``?
     }
   
     // Internal register functionality
@@ -102,8 +140,15 @@ class UIManager {
 
     }
 
+    // Calculates whether a DOM element is visible checking display, visibility, etc and returns true//false
+    // TODO This will initially be used for the register() to see the status of a pre-existing element.
+    _calcVisibility(elem: HTMLElement) {
+        // Also should check: elem.style.visibility !== 'hidden'
+        if (elem.style.visibility === 'hidden') { return false;};
+        return !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
+    }
+
 }
 
-/// TODO will this break being a singleton by doing new here?
 // make sure to call uiManager.init(engine) in your main file
 export const uiManager = new UIManager()
